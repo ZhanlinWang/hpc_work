81a79462bdf964a8e91820020bfa35e00d2a7b22
JulianWang <zhanlin_wang@126.com>
2016-03-04 14:39:05 +0800

    Better script of sqrtx.py

--- sqrtx.py
+++ sqrtx.py
@@ -2,6 +2,7 @@
 from numpy import *
 x = 2.
 s = 1.
-for k in range(60):
+for k in range(6):
+	print "Before iteration %s, s = %s" % (k,s)
 	s = 0.5 * (s + x/s)
-print(s)
+print "After %s iteration, s=%s" % (k+1,s)
